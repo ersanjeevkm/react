a8b41fecb9686456905de9e711c1c888951ec9c9
Sanjeev K M <er.sanjeev.au@gmail.com>
2022-01-28 16:07:22 +0530

    ADD: env variable

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTkHSNY1DjVUb0CIereTg830G-gxJ8ILs",
-  authDomain: "clone-5cf45.firebaseapp.com",
-  projectId: "clone-5cf45",
-  storageBucket: "clone-5cf45.appspot.com",
-  messagingSenderId: "51559984203",
-  appId: "1:51559984203:web:05ea97bf93b833518731ce",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
